95fd4e5dc792362c3af64022f6e8b70ccee3adc8
Szili Péter <szilipeti.x0@gmail.com>
2018-09-14 16:29:59 +0200

    update jwt

--- Package.swift
+++ Package.swift
@@ -9,14 +9,15 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
             .package(url: "https://github.com/IBM-Swift/Kitura.git", from: "2.0.0"),
             .package(url: "https://github.com/IBM-Swift/Swift-Kuery-ORM.git", from: "0.3.1"),
-            .package(url: "https://github.com/IBM-Swift/SwiftKueryMySQL.git", from: "1.2.0")
+            .package(url: "https://github.com/IBM-Swift/SwiftKueryMySQL.git", from: "1.2.0"),
+            .package(url: "https://github.com/vapor/jwt.git", from: "3.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "guidemeserver",
-            dependencies: ["Kitura", "SwiftKueryORM", "SwiftKueryMySQL"],
+            dependencies: ["Kitura", "SwiftKueryORM", "SwiftKueryMySQL", "JWT"],
             path: "./Sources/GuideMEServer/"),
     ]
 )
